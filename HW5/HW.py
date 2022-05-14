4681a8700b39d0e612cd35dff4ccf3899e678ab7
sergeymatkobozhyk <megawacko94@gmail.com>
2022-05-14 10:11:33 +0300

    HW5 fixed

--- HW5/HW.py
+++ HW5/HW.py
@@ -6,7 +6,8 @@ try:
         print(words)
 
         for i in words:
-            words_dict.update({i: words.count(i)})
+            words_dict.setdefault(i, 0)
+            words_dict[i] += 1
 
     most_occurent_word = max(words_dict, key=words_dict.get)
     most_occurent_word_count = words_dict[most_occurent_word]
@@ -17,17 +18,15 @@ try:
     print(most_occurent_word, f"is the most occurent, found {most_occurent_word_count} time(s)")
     print(less_occurent_word, f"is the less occurent, found {less_occurent_word_count} time(s)")
 
-    words_counter = 0
-    try:
-        with open("spam.txt", "x") as spam:
-            for word in words:
-                spam.write(word + " ")
-                words_counter += 1
-                if words_counter == 10:
-                    words_counter = 0
-                    spam.write("\n")
-    except FileExistsError:
-        raise FileExistsError("File already exists!")
+    with open("spam.txt", "x") as spam:
+        for position, word in enumerate(words, start=1):
+            spam.write(word.replace(most_occurent_word, less_occurent_word) + " ")
+
+            if position % 10 == 0:
+                spam.write("\n")
+
+except FileExistsError:
+    raise FileExistsError("File already exists!")
 
 except FileNotFoundError:
     raise FileNotFoundError("File not found! Script finished!")
