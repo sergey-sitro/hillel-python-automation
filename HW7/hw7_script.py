25f60055e0a49ff0ff82a67b118f3de3fb556260
sergeymatkobozhyk <megawacko94@gmail.com>
2022-05-21 20:55:41 +0300

    HW7 resolved

--- HW7/hw7_script.py
+++ HW7/hw7_script.py
@@ -8,27 +8,21 @@ response_json = response.json()
 
 titles = [t["title"].capitalize() for t in response_json]
 bodies = [b["body"].replace("\n", ". ") for b in response_json]
-print(bodies)
 
 list_bodies = [s.split(". ") for s in bodies]
-print(list_bodies)
 
-real_bodies = [i for i in [j for j in list_bodies]]
+groupped_bodies = [
+    [string.capitalize() for string in string_list]
+    for string_list in list_bodies
+]
 
-# for i in list_bodies:
-#     for j in i:
-#         real_bodies.append(j.capitalize())
+final_bodies = [". ".join(b) + "." for b in groupped_bodies]
 
-print(real_bodies)
+title_and_body = list(zip(titles, final_bodies))
 
+fp = "/Users/sergeymatkobozhyk/Desktop/Hillel/Hillel Project/Homework/hillel-python-automation/HW7/file.txt"
 
-# title_and_body = list(zip(titles, bodies))
-# print(title_and_body)
-#
-# fp = "/Users/sergeymatkobozhyk/Desktop/Hillel/Hillel Project/Homework/hillel-python-automation/HW7/file.txt"
-#
-# with open(fp, "w") as file:
-#     for t, b in title_and_body:
-#         file.write(t)
-#         file.write(b)
-
+with open(fp, "w") as file:
+    for t, b in title_and_body:
+        file.write(t + "\n")
+        file.write(b + "\n\n")
