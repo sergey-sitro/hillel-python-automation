9e524477785279a019b821cab00b90ea89d483a4
sergeymatkobozhyk <megawacko94@gmail.com>
2022-05-22 10:21:11 +0300

    HW7 fixed

--- HW7/hw7_script.py
+++ HW7/hw7_script.py
@@ -5,23 +5,14 @@ url = "https://jsonplaceholder.typicode.com/posts"
 response = requests.get(url).json()
 
 
-titles = [t["title"].capitalize() for t in response]
-bodies = [b["body"] for b in response]
+def get_title_and_bodies(lst):
+    content = [(i["title"].capitalize(), [j.capitalize() + ".\n" for j in i["body"].split("\n")]) for i in lst]
+    return content
 
-list_bodies = [s.split("\n") for s in bodies]
-
-groupped_bodies = [
-    [string.capitalize() + ".\n" for string in string_list]
-    for string_list in list_bodies
-]
-
-final_bodies = ["".join(b) for b in groupped_bodies]
-
-title_and_body = list(zip(titles, final_bodies))
 
 fp = "/Users/sergeymatkobozhyk/Desktop/Hillel/Hillel Project/Homework/hillel-python-automation/HW7/file.txt"
 
 with open(fp, "w") as file:
-    for t, b in title_and_body:
-        file.write(t + "\n")
-        file.write(b + "\n")
+    for t, b in get_title_and_bodies(response):
+        file.write(t + "\n\n")
+        file.write("".join(b) + "\n")
