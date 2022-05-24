5e61cb77194360bf0fa99f05835969f49656e323
sergeymatkobozhyk <megawacko94@gmail.com>
2022-05-24 22:28:06 +0300

    HW9 resolved

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -2,38 +2,101 @@ class Character:
     dead_health = 0
 
     def __init__(self, race, damage, armor):
-        self.race = race
-        self.damage = damage
-        self.armor = armor
+        try:
+            self.race = race
+
+            if not isinstance(race, str):
+                raise TypeError
+
+        except TypeError:
+            print("Race can be 'str' only!")
+            raise TypeError
+
+        try:
+            self.damage = damage
+
+            if not isinstance(damage, int):
+                raise TypeError
+            elif damage <= 0:
+                raise ValueError
+
+        except TypeError:
+            print("Damage can be int only")
+            raise TypeError
+
+        except ValueError:
+            print("Damage attribute should be > 0!")
+            raise ValueError
+
+        try:
+            self.armor = armor
+
+            if not isinstance(armor, int):
+                raise TypeError
+            elif armor < 0:
+                raise ValueError
+
+        except TypeError:
+            print("Armor can be int only!")
+            raise TypeError
+
+        except ValueError:
+            print("Armor attribute should be not less than 0!")
+            raise ValueError
+
         self.health = 100
 
-        if not isinstance(race, str) or not isinstance(damage, int) or not isinstance(armor, int):
-            raise TypeError("Wrong type!")
-        elif damage <= 0 or armor < 0:
-            raise ValueError("Wrong value!")
+    def hit(self, damage):
+        try:
+            self.health = self.health - damage + (damage / self.armor)
 
-    def hit(self, damage, armor):
-        self.health = self.health - damage + (damage / armor)
+            if not isinstance(damage, int):
+                raise TypeError
+            elif damage < 0:
+                raise ValueError
+
+        except TypeError:
+            print("Can not hit by a string!")
+            raise TypeError
+
+        except ValueError:
+            print("Can not hit less than 0 hit points!")
+            raise ValueError
 
     def restore_health(self, health):
-        self.health += health
+        try:
+            self.health += health
+
+            if not isinstance(health, int):
+                raise TypeError
+            elif health <= 0:
+                raise ValueError
+
+        except TypeError:
+            print("You can not heal by a string!")
+            raise ValueError
+
+        except ValueError:
+            print("You can't hurt yourself by healing!")
+            raise ValueError
 
     @property
-    def get_current_health(self):
+    def current_health(self):
         return self.health
 
     def is_dead(self):
-        return self.health <= Character.dead_health
+        return self.health <= self.dead_health
 
     def respawn(self):
         self.health = 100
 
 
-orc = Character("Orc", 12, 4)
+orc = Character("Orc", 10, 5)
 
-orc.hit(100, orc.armor)
-print(orc.get_current_health)
+orc.hit(100)
+print(orc.current_health)
 print(orc.is_dead())
-orc.hit(40, orc.armor)
-print(orc.get_current_health)
+orc.hit(10)
+orc.restore_health(-1)
+print(orc.current_health)
 print(orc.is_dead())
