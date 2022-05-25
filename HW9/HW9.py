0e7646523864772f08548f8d598664294407b6df
sergeymatkobozhyk <megawacko94@gmail.com>
2022-05-25 10:26:55 +0300

    HW9 resolved

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,84 +1,39 @@
 class Character:
     dead_health = 0
 
+    @staticmethod
+    def validate_attr_type(attr, attr_name, correct_type):
+        if type(attr) is not correct_type:
+            raise TypeError(f"{attr_name} must be a {correct_type}, not a {type(attr)}")
+
+    @staticmethod
+    def validate_attr_value(attr, attr_name):
+        if attr < 0:
+            raise ValueError(f"{attr_name} can not be less than 0!")
+
     def __init__(self, race, damage, armor):
-        try:
-            self.race = race
+        self.validate_attr_type(race, "Race", str)
+        self.race = race
 
-            if not isinstance(race, str):
-                raise TypeError
+        self.validate_attr_type(damage, "Damage", int)
+        self.validate_attr_value(damage, "Damage")
+        self.damage = damage
 
-        except TypeError:
-            print("Race can be 'str' only!")
-            raise TypeError
-
-        try:
-            self.damage = damage
-
-            if not isinstance(damage, int):
-                raise TypeError
-            elif damage <= 0:
-                raise ValueError
-
-        except TypeError:
-            print("Damage can be int only")
-            raise TypeError
-
-        except ValueError:
-            print("Damage attribute should be > 0!")
-            raise ValueError
-
-        try:
-            self.armor = armor
-
-            if not isinstance(armor, int):
-                raise TypeError
-            elif armor < 0:
-                raise ValueError
-
-        except TypeError:
-            print("Armor can be int only!")
-            raise TypeError
-
-        except ValueError:
-            print("Armor attribute should be not less than 0!")
-            raise ValueError
+        self.validate_attr_type(armor, "Armor", int)
+        self.validate_attr_value(armor, "Armor")
+        self.armor = armor
 
         self.health = 100
 
     def hit(self, damage):
-        try:
-            self.health = self.health - damage + (damage / self.armor)
-
-            if not isinstance(damage, int):
-                raise TypeError
-            elif damage < 0:
-                raise ValueError
-
-        except TypeError:
-            print("Can not hit by a string!")
-            raise TypeError
-
-        except ValueError:
-            print("Can not hit less than 0 hit points!")
-            raise ValueError
+        self.validate_attr_type(damage, "Hit damage", int)
+        self.validate_attr_value(damage, "Hit damage")
+        self.health = self.health - damage + (damage / self.armor)
 
     def restore_health(self, health):
-        try:
-            self.health += health
-
-            if not isinstance(health, int):
-                raise TypeError
-            elif health <= 0:
-                raise ValueError
-
-        except TypeError:
-            print("You can not heal by a string!")
-            raise ValueError
-
-        except ValueError:
-            print("You can't hurt yourself by healing!")
-            raise ValueError
+        self.validate_attr_type(health, "Restore HP", int)
+        self.validate_attr_value(health, "Restore HP")
+        self.health += health
 
     @property
     def current_health(self):
@@ -91,12 +46,12 @@ class Character:
         self.health = 100
 
 
-orc = Character("Orc", 10, 5)
+orc = Character("Orc", 1, 5)
 
 orc.hit(100)
 print(orc.current_health)
 print(orc.is_dead())
-orc.hit(10)
-orc.restore_health(-1)
+orc.hit(1)
+orc.restore_health("Str")
 print(orc.current_health)
 print(orc.is_dead())
