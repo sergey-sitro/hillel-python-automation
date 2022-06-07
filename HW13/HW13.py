1cccb7e78392546bf5cf63807533ac54d8d1aa6f
sergeymatkobozhyk <megawacko94@gmail.com>
2022-06-07 21:33:42 +0300

    HW13

---
+++ HW13/HW13.py
@@ -0,0 +1,39 @@
+# Go to https://www.saucedemo.com and log in as standard user
+
+# 1st item:
+# Find name of 1st item: //*[text()='Sauce Labs Backpack']
+# Find description of 1st item: //a[@id='item_4_title_link']/following-sibling::div
+# Find price of 1st item: //a[@id='item_4_title_link']/../following-sibling::div/div[@class='inventory_item_price']
+# Find 'Add to Cart' button of 1st item: //button[@data-test='add-to-cart-sauce-labs-backpack']
+# Find picture of 1st item: //img[@alt='Sauce Labs Backpack']
+
+# 2nd item:
+# Find name of 2nd item:
+# Find description of 2nd item:
+# Find price of 2nd item:
+# Find 'Add to Cart' button of 2nd item:
+# Find picture of 2nd item:
+
+# 3rd item:
+# Find name of 3rd item:
+# Find description of 3rd item:
+# Find price of 3rd item:
+# Find 'Add to Cart' button of 3rd item:
+# Find picture of 3rd item:
+
+# 4th item:
+# Find name of 4th item:
+# Find description of 4th item:
+# Find price of 4th item:
+# Find 'Add to Cart' button of 4th item:
+# Find picture of 4th item:
+
+# 5th item:
+# Find name of 5th item:
+# Find description of 5th item:
+# Find price of 5th item:
+# Find 'Add to Cart' button of 5th item:
+# Find picture of 5th item:
+
+
+
