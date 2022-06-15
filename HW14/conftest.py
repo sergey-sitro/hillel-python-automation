c2fd4ebfbf64dc1061e5f1ca7db8648c656eda18
sergeymatkobozhyk <megawacko94@gmail.com>
2022-06-15 21:42:12 +0300

    HW14

--- HW14/conftest.py
+++ HW14/conftest.py
@@ -19,22 +19,6 @@ def create_chrome_driver():
     chrome_driver.quit()
 
 
-# @pytest.fixture
-# def create_firefox_driver():
-#     firefox_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-#     firefox_driver.maximize_window()
-#     yield firefox_driver
-#     firefox_driver.quit()
-#
-#
-# @pytest.fixture
-# def create_safari_driver():
-#     safari_driver = webdriver.Safari()
-#     safari_driver.maximize_window()
-#     yield safari_driver
-#     safari_driver.quit()
-
-
 @pytest.fixture
 def chrome_login(create_chrome_driver):
     username = 'standard_user'
@@ -52,3 +36,60 @@ def chrome_login(create_chrome_driver):
     login_button_locator = '//*[@data-test="login-button"]'
     login_button_element = create_chrome_driver.find_element(By.XPATH, login_button_locator)
     login_button_element.click()
+    return create_chrome_driver
+
+
+@pytest.fixture
+def create_firefox_driver():
+    firefox_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    firefox_driver.maximize_window()
+    yield firefox_driver
+    firefox_driver.quit()
+
+
+@pytest.fixture
+def firefox_login(create_firefox_driver):
+    username = 'standard_user'
+    password = 'secret_sauce'
+    create_firefox_driver.get(URL)
+    create_firefox_driver.maximize_window()
+    login_input_locator = '//*[@data-test="username"]'
+    login_input_element = create_firefox_driver.find_element(By.XPATH, login_input_locator)
+    password_input_locator = '//*[@data-test="password"]'
+    password_input_element = create_firefox_driver.find_element(By.XPATH, password_input_locator)
+    time.sleep(3)
+    login_input_element.send_keys(username)
+    password_input_element.send_keys(password)
+    time.sleep(3)
+    login_button_locator = '//*[@data-test="login-button"]'
+    login_button_element = create_firefox_driver.find_element(By.XPATH, login_button_locator)
+    login_button_element.click()
+    return create_firefox_driver
+
+
+@pytest.fixture
+def create_safari_driver():
+    safari_driver = webdriver.Safari()
+    safari_driver.maximize_window()
+    yield safari_driver
+    safari_driver.quit()
+
+
+@pytest.fixture
+def safari_login(create_safari_driver):
+    username = 'standard_user'
+    password = 'secret_sauce'
+    create_safari_driver.get(URL)
+    create_safari_driver.maximize_window()
+    login_input_locator = '//*[@data-test="username"]'
+    login_input_element = create_safari_driver.find_element(By.XPATH, login_input_locator)
+    password_input_locator = '//*[@data-test="password"]'
+    password_input_element = create_safari_driver.find_element(By.XPATH, password_input_locator)
+    time.sleep(3)
+    login_input_element.send_keys(username)
+    password_input_element.send_keys(password)
+    time.sleep(3)
+    login_button_locator = '//*[@data-test="login-button"]'
+    login_button_element = create_safari_driver.find_element(By.XPATH, login_button_locator)
+    login_button_element.click()
+    return create_safari_driver
