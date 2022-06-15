ed4842f70a2af0b843045381ade41b8d2d021fd3
sergeymatkobozhyk <megawacko94@gmail.com>
2022-06-15 14:07:20 +0300

    HW14

--- HW14/conftest.py
+++ HW14/conftest.py
@@ -1,18 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
-
-import os
-os.environ['GH_TOKEN'] = "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"
 
 
 @pytest.fixture()
 def create_chrome_driver():
-    chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     chrome_driver.maximize_window()
     yield chrome_driver
     chrome_driver.quit()
@@ -20,7 +17,7 @@ def create_chrome_driver():
 
 @pytest.fixture()
 def create_firefox_driver():
-    firefox_driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    firefox_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     firefox_driver.maximize_window()
     yield firefox_driver
     firefox_driver.quit()
