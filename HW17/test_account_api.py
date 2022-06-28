976837e1d60fe5e1f39d1e7453e4ed31bd659750
sergeymatkobozhyk <megawacko94@gmail.com>
2022-06-28 12:59:53 +0300

    HW17

--- HW17/test_account_api.py
+++ HW17/test_account_api.py
@@ -8,6 +8,6 @@ def test_post_user(register):
     req_username = response.request.body.split("&")[0].replace("userName=", "")
     print(req_username)
     assert response.status_code == HTTPStatus.CREATED
+    assert json.loads(response.text)["userID"]
     assert req_username == json.loads(response.text)["username"]
     assert json.loads(response.text)["books"] == []
-    assert json.loads(response.text)["userID"]
