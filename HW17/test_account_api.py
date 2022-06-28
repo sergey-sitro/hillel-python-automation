0b52a45e8459958c837bc4b35c984fa1b17de947
sergeymatkobozhyk <megawacko94@gmail.com>
2022-06-28 12:58:33 +0300

    HW17

--- HW17/test_account_api.py
+++ HW17/test_account_api.py
@@ -1,7 +1,13 @@
+import json
 from http import HTTPStatus
 
 
 def test_post_user(register):
     response = register
     print("\n"+response.text)       # delete!!!!!
+    req_username = response.request.body.split("&")[0].replace("userName=", "")
+    print(req_username)
     assert response.status_code == HTTPStatus.CREATED
+    assert req_username == json.loads(response.text)["username"]
+    assert json.loads(response.text)["books"] == []
+    assert json.loads(response.text)["userID"]
